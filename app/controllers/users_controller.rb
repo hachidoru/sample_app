a8778928f6def7c216ab6c00d1496720538a0d13
hachidoru <hachidoru@gmail.com>
2015-05-27 19:56:00 +0900

    Exercise 9.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :unsigned_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,9 +40,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    user = User.find(params[:id])
+    if user.admin?
+      redirect_to root_path
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    end
   end
 
   private
@@ -58,6 +64,10 @@ class UsersController < ApplicationController
         store_location
 	redirect_to signin_url, notice: "Please sign in."
       end
+    end
+
+    def unsigned_in_user
+      redirect_to(root_path) if signed_in?
     end
 
     def correct_user
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+
+    it { should_not have_link('Users') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+    it { should_not have_link('Sign out') }
   end
 
   describe "signin" do
@@ -58,8 +63,23 @@ describe "Authentication" do
 	end
 
 	describe "after signing in" do
+
 	  it "should render the desired protected page" do
 	    expect(page).to have_title('Edit user')
+	  end
+
+	  describe "when signing in again" do
+	    before do
+	      delete signout_path
+	      visit signin_path
+	      fill_in "Email", with: user.email
+	      fill_in "Password", with: user.password
+	      click_button "Sign in"
+	    end
+
+	    it "should render the default (profile) page" do
+	      expect(page).to have_title(user.name)
+	    end
 	  end
 	end
       end
@@ -110,6 +130,31 @@ describe "Authentication" do
         before { delete user_path(user) }
 	specify { expect(response).to redirect_to(root_path) }
       end
+    end
+
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before { sign_in admin, no_capybara: true }
+
+      it "submitting a DELETE request to the Users#destroy on myself(admin)" do
+        expect { delete user_path(admin) }.not_to change(User, :count)
+      end
+    end
+
+    describe "as signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting GET request to the Users#new action" do
+        before { get new_user_path }
+	specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting POST request to the Users#create action" do
+        before { post users_path }
+	specify { expect(response).to redirect_to(root_url) }
+      end
+
     end
   end
 
